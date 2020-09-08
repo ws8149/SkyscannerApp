04ebb7bd1a0a4d3e6f2531acb40425c6f175151d
ws8149 <weisheng3725@gmail.com>
2020-09-08 12:35:08 +0800

    add one way switch

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -6,6 +6,7 @@ import { Input } from 'react-native-elements';
 import { Text } from 'react-native-elements';
 import { Button } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
+import { Switch } from 'react-native-gesture-handler';
 
 
 const SearchContainer = styled.View`
@@ -17,16 +18,27 @@ const ButtonContainer = styled.View`
   justify-content: space-evenly;  
 `
 
-const StyledInput = styled(Input).attrs({
-  textAlign: 'right',  
-})``;
+const SwitchContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-evenly;  
+  margin-bottom: 15px;
+`
+
+const OneWaySwitch = styled.Switch`
+  
+`
+
+const SwitchText = styled.Text`
+  margin-top: 5px;
+  font-size: 18px;
+`
 
 
 
 
 const SearchScreen: React.FC = () => {   
   const navigation = useNavigation();
-
+  const [isOneWay, setIsOneWay] = useState(false)
   const [searchParams, setState] = useState({
     departureAirport: '',
     destinationAirport: '',
@@ -40,6 +52,10 @@ const SearchScreen: React.FC = () => {
     })
   }
 
+  const toggleSwitch = () => {
+    setIsOneWay(prevState => !prevState)
+  }
+
   return (
     <SearchContainer>      
       <Input         
@@ -51,10 +67,21 @@ const SearchScreen: React.FC = () => {
       <Input 
         placeholder='Departure Date (YYYY/MM/DD)'
         onChangeText={text => setState({...searchParams, departureDate: text})}/>        
-      <Input 
+      { isOneWay ? <View/> : (
+        <Input 
         placeholder='Return Date (YYYY/MM/DD)'
         onChangeText={text => setState({...searchParams, returnDate: text})}/>        
-      
+      )}      
+      <SwitchContainer>
+        <SwitchText>One Way</SwitchText>
+        <Switch
+          trackColor={{ false: "white", true: "blue" }}
+          // thumbColor={isOneWay ? "#white" : "white"}
+          onValueChange={toggleSwitch}
+          value={isOneWay}
+          
+        />
+      </SwitchContainer>
       <Button title="Search" onPress={search} />
       
     </SearchContainer>
